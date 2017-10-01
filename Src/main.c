d6b80765324a38308fedd6dabb8e181fc8b41301
TsaiFuMin <tsaifuming0205@gmail.com>
2017-10-01 23:43:51 +0800

    2017/10/1-23:43

--- Src/main.c
+++ Src/main.c
@@ -166,7 +166,7 @@ int main(void)
 
   /* USER CODE BEGIN 3 */
 
-    
+    Get_Data(house_temp);
 		
     IO_CHK_Routine();       //DO NOT EDIT
 
@@ -412,8 +412,6 @@ void CMP_CHK_Routine(void){
   uint8_t alarm_sta;
   alarm_sta=IO_Status.Comp_OVP&&IO_Status.LV&&IO_Status.HV&&IO_Status.Oil;
 
-  //Temporary
-  Temp.Read_House_Temp_int=0;
   static uint8_t house_temp_cmp_sta;
   if(Temp.Read_House_Temp_int<Temp.house_temp_int){
     house_temp_cmp_sta=0; //0 = turn off comp
@@ -447,8 +445,6 @@ void CMP_CHK_Routine(void){
   }
 
 }
-
-
 
 void Refri_CHK_Routine(void){
 
